ffed179d704c84dff38ef919f1ba8b120d931cf3
NikitaKrylov <idoverchiviiloh@gmail.com>
2024-02-21 02:32:22 +0300

    fix docker compose

--- app/src/users/models.py
+++ app/src/users/models.py
@@ -13,3 +13,15 @@ class User(Base):
     age: Mapped[int | None] = mapped_column(default=None)
 
 
+class DonationRequest(Base):
+    __tablename__ = 'donation_requests'
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    pet_type: Mapped[str]
+    age: Mapped[int]
+    is_first: Mapped[bool] = mapped_column(default=True)
+
+
+
+
+
