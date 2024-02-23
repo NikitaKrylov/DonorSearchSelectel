d6294abf5f0acbfbd29be8cde009b3d8d5d56bd1
paulineryabuhina <polina.ryabukhina1@gmail.com>
2024-02-23 13:04:51 +0300

    home

--- front_vite/src/pages/Home/Home.jsx
+++ front_vite/src/pages/Home/Home.jsx
@@ -1,20 +1,15 @@
 import './Home.scss';
 import Header from '../../components/Header/index.jsx';
-<<<<<<< HEAD
 import Banner from '../../components/Banner/index.jsx'
 import { Pets } from '../../components/Card/utils.js';
 import Card from '../../components/Card/Card.jsx';
 
-=======
-import Banner from '../../components/Banner/index.jsx';
->>>>>>> 385cf7a60ddd799c559f38378c54166707217273
 
 const Home = () => {
     return (
         <div className='home'>
             <Header />
             <Banner />
-<<<<<<< HEAD
             <h2 className='home__title'>Им подарили новую жизнь</h2>
             <div className='home__cards'>
                 {
@@ -26,8 +21,6 @@ const Home = () => {
                 }
             </div>
 
-=======
->>>>>>> 385cf7a60ddd799c559f38378c54166707217273
         </div>
         
     )
