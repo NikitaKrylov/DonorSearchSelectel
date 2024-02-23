85fe2917bc0e90dcd335ce4e4093b39127f0dc02
Osokin Yaroslav <osyaros.2005@yandex.ru>
2024-02-23 13:07:58 +0300

    Acc

--- front_vite/src/pages/Account/Account.jsx
+++ front_vite/src/pages/Account/Account.jsx
@@ -1,14 +1,18 @@
-import './Account.scss';
-import Header from '../../components/Header/index.jsx';
-import Profile from '../../components/Profile/Profile.jsx';
-
-const Account = () => {
-    return (
-        <div>
-            <Header />
-            <Profile />
-        </div>
-    );
-}
+import './Account.scss'; 
+import Header from '../../components/Header/index.jsx'; 
+import Profile from '../../components/Profile/Profile.jsx'; 
+import PetsProfile from '../../components/PetsProfile/PetsProfile.jsx'; 
  
+const Account = () => { 
+    return ( 
+        <div className='account'> 
+            <Header /> 
+            <div className='account__profile'> 
+                <Profile /> 
+                <PetsProfile /> 
+            </div> 
+        </div> 
+    ); 
+} 
+  
 export default Account;
